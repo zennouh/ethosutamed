a96df1ce335467d15ce6c28edb94635d089104e1
nouh <nouh.nowile@gmail.com>
2025-10-16 19:37:13 +0100

    end task in youcode

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
-import { src, dest, watch, series } from 'gulp'
+import { src, dest, watch, series } from "gulp";
 
-import * as dartSass from 'sass'
+import * as dartSass from "sass";
 
-import gulpSass from 'gulp-sass'
+import gulpSass from "gulp-sass";
 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
 
 function buildStyles() {
-  return src('sass/character.scss').pipe(sass()).pipe(dest('styles'))
+  return src([
+    "sass/character/character.scss",
+    "sass/gun/gun.scss",
+    "sass/reword/reword.scss",
+  ])
+    .pipe(sass())
+    .pipe(dest("styles"));
 }
 
 function WatchMe() {
-  watch(['sass/character.scss'], buildStyles)
+  watch(
+    [
+      "sass/character/character.scss",
+      "sass/gun/gun.scss",
+      "sass/reword/reword.scss",
+    ],
+    buildStyles
+  );
 }
 
-export default series(buildStyles, WatchMe)
+export default series(buildStyles, WatchMe);
